fbb56797d1ff871edab18af0012cf55f32024206
hiblurryface <AlexandrKsenz@gmail.com>
2021-04-16 19:37:28 +0300

    master

--- js/script.js
+++ js/script.js
@@ -3,3 +3,25 @@ $(document).ready(function () {
         $('.header__menu').toggleClass('active');
     })
 })
+
+window.onscroll = function showHeader() {
+    var header = document.querySelector('.header');
+    if (window.pageYOffset > 1) {
+        header.classList.add('header_active');
+    } else {
+        header.classList.remove('header_active');
+    }
+}
+
+new Swiper('.review__slider', {
+    navigation: {
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    simulateTouch: false,
+    spaceBetween: 50,
+});
